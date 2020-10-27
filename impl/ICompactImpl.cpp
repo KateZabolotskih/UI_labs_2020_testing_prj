b0156cbc08e05c735ebfecb164845a6d2ee8a62a
Kate Zabolotskih <zabolotskihkate@gmail.com>
2020-10-27 16:06:41 +0300

    implementation is done

--- impl/ICompactImpl.cpp
+++ impl/ICompactImpl.cpp
@@ -4,40 +4,37 @@
 #include <algorithm>// min, max
 #include <assert.h> // assert
 namespace {
+    enum SEQUENCE {INVERSE = -1, EXPLICIT = 1};
     /* declaration */
     class ICompactImpl : public ICompact {
     private:
-        size_t m_dim {0};
-        IVector * m_begin {nullptr};
-        IVector * m_end {nullptr};
-        ILogger * m_logger {nullptr};
-        double const m_tolerance;
+        size_t _dim {0};
+        IVector * _begin {nullptr};
+        IVector * _end {nullptr};
+        ILogger * _logger {nullptr};
+        double const _accuracy;
 
     public:
         class IteratorImpl : public ICompact::Iterator {
         private:
-            IVector const * m_begin {nullptr};
-            IVector const * m_end {nullptr};
-            IVector const * m_step {nullptr};
-            IVector * m_current {nullptr};
-            std::vector<size_t> m_direction;
-            // true  - direct   traversal
-            // false - reversed traversal
-            bool m_traversal;
-            ILogger * m_logger {nullptr};
-
+            IVector const * _begin {nullptr};
+            IVector const * _end {nullptr};
+            IVector const * _step {nullptr};
+            IVector * _cur_point {nullptr};
+            std::vector<size_t> _direction;
+            SEQUENCE _orientation;
+            ILogger * _logger {nullptr};
         public:
             ReturnCode setDirection(std::vector<size_t> const & direction)  override;
             ReturnCode doStep()                                             override;
             IVector * getPoint()                                      const override;
 
-            IteratorImpl(IVector const * beg, IVector const * end, IVector const * step, std::vector<size_t> const & dir, bool traversal);
+            IteratorImpl(IVector const * beg, IVector const * end, IVector const * step, std::vector<size_t> const & dir, SEQUENCE orientation);
             ~IteratorImpl();
         };
 
-        Iterator * begin(IVector const * step) override;
-        Iterator * end(IVector const * step)   override;
-
+        Iterator * begin(IVector const * step)                            override;
+        Iterator * end(IVector const * step)                              override;
         ICompact * clone()                                          const override;
         IVector * getBegin()                                        const override;
         IVector * getEnd()                                          const override;
@@ -51,82 +48,167 @@ namespace {
     };
 }
 
-/* implementation */
-ICompactImpl::IteratorImpl::IteratorImpl(
-        IVector const * begin,
-        IVector const * end,
-        IVector const * step,
-        std::vector<size_t> const & direction,
-        bool traversal) :
-        m_begin(begin),
-        m_end(end),
-        m_step(step),
-        m_current(traversal ? begin->clone() : end->clone()),
-        m_direction(direction),
-        m_traversal(traversal) {
-    m_logger = ILogger::createLogger(this);
+ICompactImpl::IteratorImpl::IteratorImpl(IVector const * begin,
+                                         IVector const * end,
+                                         IVector const * step,
+                                         std::vector<size_t> const & direction,
+                                         SEQUENCE orientation) :
+        _begin(begin),
+        _end(end),
+        _step(step),
+        _direction(direction),
+        _orientation(orientation) {
+    if (_orientation == EXPLICIT) {
+        _cur_point = begin->clone();
+    } else {
+        _cur_point = end->clone();
+    }
+    _logger = ILogger::createLogger(this);
 }
 
 ICompactImpl::IteratorImpl::~IteratorImpl() {
-    if (m_begin != nullptr) {
-        delete m_begin;
-        m_begin = nullptr;
+    if (_begin != nullptr) {
+        delete _begin;
+        _begin = nullptr;
     }
-    if (m_end != nullptr) {
-        delete m_end;
-        m_end = nullptr;
+    if (_end != nullptr) {
+        delete _end;
+        _end = nullptr;
     }
-    if (m_current != nullptr) {
-        delete m_current;
-        m_current = nullptr;
+    if (_cur_point!= nullptr) {
+        delete _cur_point;
+        _cur_point = nullptr;
     }
-    if (m_step != nullptr) {
-        delete m_step;
-        m_step = nullptr;
+    if (_step != nullptr) {
+        delete _step;
+        _step = nullptr;
     }
-    if (m_logger != nullptr) {
-        m_logger->releaseLogger(this);
+    if (_logger != nullptr) {
+        _logger->releaseLogger(this);
     }
-}
-
-ReturnCode ICompactImpl::IteratorImpl::doStep() {
-
 }
 
 ReturnCode ICompactImpl::IteratorImpl::setDirection(std::vector<size_t> const & direction) {
+    if (direction.size() != _begin->getDim()) {
+        LOG(_logger, ReturnCode::RC_WRONG_DIM);
+    }
+    std::vector<bool> bool_vec(direction.size(), false);
+    for (auto dir : direction) {
+        bool_vec[dir] = true;
+    }
+    bool have_all_dirs = true;
+    for (auto exist_axis : bool_vec) {
+        if (!exist_axis) {
+            have_all_dirs = false;
+            break;
+        }
+    }
+    if (!have_all_dirs) {
+        LOG(_logger, ReturnCode::RC_INVALID_PARAMS);
+    }
+    _direction = direction;
+    return ReturnCode::RC_SUCCESS;
+}
 
+ReturnCode ICompactImpl::IteratorImpl::doStep() {
+    size_t cur_axis = 0;
+    double new_value;
+    for (size_t i = 0; i < _begin->getDim(); i++) {
+        new_value = _cur_point->getCoord(i) + _orientation * _step->getCoord(i);
+        if (_orientation == INVERSE && new_value < _begin->getCoord(i)) {
+            _cur_point->setCoord(i, _end->getCoord(i));
+        }
+        if (_orientation == EXPLICIT && new_value > _end->getCoord(i)) {
+            _cur_point->setCoord(i, _begin->getCoord(i));
+        }
+        else {
+            _cur_point->setCoord(i, new_value);
+            cur_axis = i;
+            break;
+        }
+    }
+
+    if (cur_axis >= _begin->getDim()) {
+        LOG(_logger, ReturnCode::RC_OUT_OF_BOUNDS);
+        delete _cur_point;
+        if (_orientation == EXPLICIT) {
+            _cur_point = _end->clone();
+        } else {
+            _cur_point = _begin->clone();
+        }
+        return ReturnCode::RC_OUT_OF_BOUNDS;
+    }
+    return ReturnCode::RC_SUCCESS;
 }
 
 IVector * ICompactImpl::IteratorImpl::getPoint() const {
-    return m_current->clone();
+    return _cur_point->clone();
 }
 
-static IVector * buildStep(IVector const * temp, size_t dim, ILogger * logger) {
-
-}
-
-static ICompact::Iterator * createIterator(IVector const * _begin, IVector const * _end, IVector const * temp, bool traversal, ILogger * logger) {
+static IVector * buildStep(IVector const * step, size_t dim, ILogger * logger) {
 
 }
 
-ICompact::Iterator * ICompactImpl::begin(IVector const * temp) {
-    return createIterator(m_begin, m_end, temp, true, m_logger);
+static ICompact::Iterator* createIterator(IVector const * _begin, IVector const * _end, IVector const * _step, SEQUENCE orientation, ILogger * logger) {
+    if (_begin == nullptr || _end == nullptr || _step == nullptr) {
+        LOG(logger, ReturnCode::RC_NULL_PTR);
+        return nullptr;
+    }
+
+    IVector const * begin = _begin->clone();
+    IVector const * end   = _end->clone();
+    if (begin == nullptr) {
+        LOG(logger, ReturnCode::RC_NULL_PTR);
+        delete begin;
+        return nullptr;
+    }
+    if (end == nullptr) {
+        LOG(logger, ReturnCode::RC_NULL_PTR);
+        delete end;
+        return nullptr;
+    }
+
+    IVector * step = buildStep(_step, begin->getDim(), logger);
+    if (step == nullptr) {
+        delete begin;
+        delete end;
+        return nullptr;
+    }
+
+    std::vector<size_t> direction(begin->getDim());
+    for (size_t cur_axis = 0; cur_axis < begin->getDim(); cur_axis++) {
+        direction[cur_axis] = cur_axis;
+    }
+
+    ICompact::Iterator * iterator = new(std::nothrow) ICompactImpl::IteratorImpl(begin, end, step, direction, orientation);
+    if (iterator == nullptr) {
+        delete begin;
+        delete end;
+        delete step;
+    }
+
+    return iterator;
 }
 
-ICompact::Iterator * ICompactImpl::end(IVector const * temp) {
-    return createIterator(m_begin, m_end, temp, false, m_logger);
+
+ICompact::Iterator * ICompactImpl::begin(IVector const * step) {
+    return createIterator(_begin, _end, step, EXPLICIT, _logger);
+}
+
+ICompact::Iterator * ICompactImpl::end(IVector const * step) {
+    return createIterator(_begin, _end, step, INVERSE, _logger);
 }
 
 ICompact* ICompactImpl::clone() const {
-    return ICompact::createCompact(m_begin, m_end, m_tolerance, m_logger);
+    return ICompact::createCompact(_begin, _end, _accuracy, _logger);
 }
 
 IVector * ICompactImpl::getBegin() const {
-    return m_begin->clone();
+    return _begin->clone();
 }
 
 IVector * ICompactImpl::getEnd() const {
-    return m_end->clone();
+    return _end->clone();
 }
 
 ReturnCode ICompactImpl::contains(IVector const * vec, bool & result) const {
@@ -142,29 +224,28 @@ ReturnCode ICompactImpl::intersects(ICompact const * other, bool & result) const
 }
 
 size_t ICompactImpl::getDim() const {
-    return m_dim;
+    return _dim;
 }
 
 ICompactImpl::ICompactImpl(IVector const * begin, IVector const * end, double accuracy) :
-        m_dim(begin->getDim()),
-        m_begin(begin->clone()),
-        m_end(end->clone()),
-        m_tolerance(accuracy) {
-    m_logger = ILogger::createLogger(this);
+        _dim(begin->getDim()),
+        _begin(begin->clone()),
+        _end(end->clone()),
+        _accuracy(accuracy) {
+    _logger = ILogger::createLogger(this);
 }
 
 ICompactImpl::~ICompactImpl() {
-    m_dim = 0;
-
-    if (m_begin != nullptr) {
-        delete m_begin;
-        m_begin = nullptr;
+    _dim = 0;
+    if (_begin != nullptr) {
+        delete _begin;
+        _begin = nullptr;
     }
-    if (m_end != nullptr) {
-        delete m_end;
-        m_end = nullptr;
+    if (_end != nullptr) {
+        delete _end;
+        _end = nullptr;
     }
-    if (m_logger != nullptr) {
-        m_logger->releaseLogger(this);
+    if (_logger != nullptr) {
+        _logger->releaseLogger(this);
     }
 }
